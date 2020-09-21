dace6d03a418a06e04d9785a5c6bc23ca5bd3a53
juneju-darad <djuneju@gmail.com>
2020-09-21 23:39:10 +0600

    AdMob dependencies added

--- app/src/main/java/com/example/adrendomizer/MainActivity.java
+++ app/src/main/java/com/example/adrendomizer/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(context: this, new OnInitializationCompleteListener() {
+            @Override
+            public void OnInitializationComplete (InitializationStatus initializationStatus)
+        }
+
+    });
     }
 }
