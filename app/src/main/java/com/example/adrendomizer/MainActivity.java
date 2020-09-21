c2d598dca52fa5140ff829c11c9ebbe46a792d4f
juneju-darad <djuneju@gmail.com>
2020-09-22 00:10:33 +0600

    AdMob Implemented

--- app/src/main/java/com/example/adrendomizer/MainActivity.java
+++ app/src/main/java/com/example/adrendomizer/MainActivity.java
@@ -2,24 +2,35 @@ package com.example.adrendomizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    AdView adView;
+    private Object AdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MobileAds.initialize(context: this, new OnInitializationCompleteListener() {
+        MobileAds.initialize( this, new OnInitializationCompleteListener() {
             @Override
-            public void OnInitializationComplete (InitializationStatus initializationStatus)
-        }
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
 
-    });
+            }
+        });
+
+        AdView = findViewById(R.id.mainBanner);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
     }
 }
